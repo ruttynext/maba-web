1df77b54a61b1d012d6ea79da825d1f9319f2a95
ruttynext <ruttynext@gmail.com>
2022-09-19 14:28:43 +0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from '.src/reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
   import '@progress/kendo-theme-default/dist/all.css';
 
