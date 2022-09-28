93bc2a11a0f51a8fcfc3357a03379c32423ecb57
ruttynext <ruttynext@gmail.com>
2022-09-28 09:03:30 +0300

    Some changes in the oven model

--- src/App.js
+++ src/App.js
@@ -7,11 +7,9 @@ import HeaderPage from './components/HeaderPage/HeaderPage';
 import MainNavbar from './components/MainNavbar/MainNavbar';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {reactLocalStorage} from 'reactjs-localstorage';
-import Oven from './Pages/Module/Oven/Oven';
-import OvenTest from './Pages/Module/OvenTest/OvenTest';
-import OvenTest2 from './Pages/Module/OvenTest/OvenTest2';
 import OvenTest3 from './Pages/Module/OvenTest/OvenTest3';
 import { ReferenceLine } from 'recharts';
+
 
 function App() { 
   const [activeUser, setActiveUser] = useState(reactLocalStorage.getObject('activeUser'));
@@ -35,15 +33,11 @@ function App() {
       <React.Fragment>
        <BrowserRouter> 
        <HeaderPage activeUser={activeUser} onLogout={(user) => onLogout(null)} ></HeaderPage>
-       <OvenTest3></OvenTest3>
-       {/* {activeUser ? <MainNavbar ></MainNavbar> : ''} */}
+        {activeUser ? <MainNavbar ></MainNavbar> : ''} 
          <Routes>
               <Route exact={true} path="/" element={<HomePage></HomePage>}/>
-              <Route exact={true} path="/login" element={<LoginPage path="/login" activeUser={activeUser} onLogin={user => onLogin(user)}/>}/>
+              <Route exact={true} path="login" element={<LoginPage path="/login" activeUser={activeUser} onLogin={user => onLogin(user)}/>}/>
               <Route exact={true} path="/SiteManagement" element={<SiteManagement/>}/>
-              <Route exact={true} path="/Oven" element={<Oven/>}/>
-              <Route exact={true} path="/OvenTest" element={<OvenTest/>}/>
-              <Route exact={true} path="/OvenTest2" element={<OvenTest2/>}/>
               <Route exact={true} path="/OvenTest3" element={<OvenTest3></OvenTest3>}></Route>
               <Route exact={true} path="/ReferenceLine" element={<ReferenceLine/>}/>
           </Routes>
--- src/Pages/Module/OvenTest/OvenTest3.jsx
+++ src/Pages/Module/OvenTest/OvenTest3.jsx
@@ -15,7 +15,8 @@ import OvenRangeData from '../../../data/OvenRange.json';
 import HydraData from '../../../data/HydraData.json';
 import './OvenTest3.css'
 import Referenceline from '../../../components/referenceline/referenceline';
-import SamplePDF from "./220837062.pdf";
+import SignWeb from '../../../components/SignForm/SignWeb';
+
 import ReportOvenImage from "./ReportOvenImage.PNG";
 //import { Document, Page } from 'react-pdf';
 
@@ -135,12 +136,13 @@ const columns = [
         {
             displayReport ? 
             <div className='viewReportDialoug'>
-            <div>
-                <Button onClick={(e) => setDisplayReport(false)}><span className="k-icon k-i-close"></span></Button>    
-                  
-                <img src={ReportOvenImage}/> 
-            </div>
-        </div> : ""
+                <div>
+                    <div className='Div-close-view-report-dialoug'>
+                        <Button onClick={(e) => setDisplayReport(false)} themeColor={"info"}><span className="k-icon k-i-close"></span></Button>                     
+                    </div>
+                    <img src={ReportOvenImage}/> 
+                </div>
+           </div> : ""
         }
       </>
     );
@@ -150,7 +152,7 @@ function OvenTest(props) {
 
     const [ certificate, SetCertificate ] = useState({});
     const [ firstPage, SetFirstPage ] = useState(true);
-
+    const [calibratorSignature, SetCalibratorSignature] = useState("iVBORw0KGgoAAAANSUhEUgAAAfQAAABkCAYAAABwx8J9AAAAAXNSR0IArs4c6QAADaRJREFUeF7tnS1UFVsUx89rREjSlAZNGjRomMCEJLFhEpKQkAQmJIEJSGKjoQlN2DTapEGEZuOtPe8dPV653Jk7+8ycj9+s5TIwd5+9f3vgf/ee8/HPzc3NlDFG/nFBAAIQgAAEIBApgX9ubm5eGWPWI/UftyEAAQhAAAIQMMYg6DwGEIAABCAAgQQIIOgJJJEQIAABCEAAAgg6zwAEIAABCEAgAQIIegJJJAQIhErg+/fv5vLy8k73BgcHzcOHD0MNAb8gEA0BBD2aVOEoBOIi8OrVK7OxsVHK6ampKbO+vm7kfy4IQKA/Agh6f9z4FAQgcAcBK+ZSec/Ozt7J6vr62hwcHJirqyszPDxs5ubmzPz8POLOEwaBigQQ9IrAuB0CELibwLdv38z4+HjRRj89PTXSUu91iZjv7e2Zw8NDI216uajae1Hj5xD4kwCCzhMBAQioEnj8+LH58OGD+fHjR1FxV71E0EXYReBF6Hd3d83S0lJVM9wPgewIIOjZpZyAIeCPgIjx2NiYWV5eNtvb27UGEjEXW3LJl4OBgYFa9vgwBFIngKCnnmHig0CDBL58+WImJyfN5uamWV1drT3ymzdvzMrKCu332iQxkAMBBD2HLBMjBBoi8PPnTzMyMqJaVcsEu52dnaL9fnJyYmZmZhqKhmEgEBcBBD2ufOEtBIInYKvqd+/emSdPnqj4ayt/sSd2uSAAgb8JIOg8FRCAgCqBT58+menpabO/v28WFxfVbD969KiYbKf5RUHNOQxBIAACCHoAScAFCKREQPs9umXj64tCSuyJJW8CCHre+Sd6CKgT8PEeXZxE0NVThcHECCDoiSWUcCAQAgH7Hl2WrskSNo3LVv7arXwN37ABgRAIIOghZAEfIJAYAV9Vumw2IxPjyuw+lxhSwoFATwIIek9E3AABCPRDwFbp7PTWDz0+A4HqBBD06sz4BASyJSA7wUn1Xea4U7lvaGjITExMFHu6c0EAAn4JIOh++WIdAkkQECGXQ1cWFhaKeMouHZP7j46OzMXFRV/7uicBjyAg0BABBL0h0AwDgRgJ2INStra2frkv77CljV7mPbaIuYi6rEeXyWxcEICAPwIIuj+2WIZAtAQ6hdyeay4HpMjJZ2XEXIKXtrs9fQ1Rj/ZxwPFICCDokSQKNyHQFAHZO31jY6MYToR8fn6+koh3+umKuuYyNi0e8uXl8vLyL3PypaXMXAEtP7ADgboEEPS6BPk8BBIiYI8/1RByF4uvZWx10MucAPnicnx83NXM1NSUWV9fL05744JA6AQQ9NAzhH8QaJCAz93Y7DK2tlvvrpDLKwTx5969e39Rvr6+NgcHB8Upb2373OAjwFARE0DQI04erkNAm4BPQQ/hfbp9nWDnArx8+fLO2fci5jKpTw6FQdS1nzbsaRNA0LWJYg8CERPwfUypK+pNbzhjxVxeJ8i56sPDw6UyFfocgFJBcFMWBBD0LNJMkBAoT8AeU+prAlsbG864Yi6b3JSdpW+phTgHoHxGuTMXAgh6LpkmTgiUJNCEeDW54Yw70a8fMbfYfBw4UzIl3AaBUgQQ9FKYuAkC7RO4bXmVr6VVVrx8zfJucsMZrXkBTXzRaf8pw4OYCSDoMWcP37Mg0Gt5lYjuixcvijaypsBLm3pnZ8fLLO8mJ8itra0Z2elO49hVqvQsfuWiDRJBjzZ1eTrebRMQS0NT0LoRtj40MZY7K/u25VXu0irrr+ZsbJ+zvF1Rl7XeEqv2pdVu5126dmaw54MAgu6DKjbVCdy2p3i3QXxVrDKeFQg7dtlDSqoCcavyXrOyRXTteunPnz8baTFrtsp9VtNie3x83Jyfn5uzszP1ndm02u1u/qjSqz7N3N8UAQS9KdKM0zcB+WM/MjJSfN7uKd7N2G0Vq6a42WVd4oeI7ubmplldXe07tts+2LlWWqrXsrOyRSClvazdKvcp6i5TEXVZI651+RB03qVrZQc72gQQdG2i2PNCQKoi+UNa5mAQt2J1BV5L2Pf29syDBw+MLO+SS7NKd1vEVdZKd0L30Sr3Ker2Pbd2692HoAtrW6Vr5t7LLw5GsyKAoGeV7jyDFXGTP8DaVWuVLxllydtqVWMClw8Bdm1OTEwUE81GR0fLhtf1Pl+td8tTu5PS+UVBDncpu1FNbVgYgEAXAgg6j0Y2BHxUrT7gSQdAzhwv22a/ywdfov78+fPivb20x6Wq1njt4KP17qs97gq6fLEZGxszvjbi8fGMYTNNAgh6mnklqi4EfAhc6LDdmDVbxLK/+bNnz4qjR7Wqddt617Lnqz3uCrp0KCYnJ9Vfv4T+XOFfeAQQ9PBygkeeCeS4N7evd8nS9VhZWVGr1iU32tW/j/3pO3nKTH2ZJKnxqsTz44/5hAkg6Aknl9C6E/DVig2VuS9Bt/FqV+uuPY2JcnZ/eq01+i5PmSA5PT1drMCos7VsqM8OfsVDAEGPJ1d4qkwglPO5lcO61Zzmbmnd/NWu1sWetLI11qhrv2qxPEXMxT+5qM6beJIZ4y4CCDrPR7YEQm69uzvi1dmRzt2Qp6kKUrNa15wopzWXwN1cSCYFStV///79Uksqs/1lI/BGCCDojWBmkFAJhNZ677YjnojG06dPK2H8+PFjscmMXE2JuXVQs1q31bBGu7zO+3TJjVTjsvxRvrT02sGvUrK4GQIKBBB0BYiYaI+AxvrfULbydCs/d0c8u51rP5TFzvz8fGvVo1utyzrtubm5wh/Z5Kfspd0ud9+ny5ekbh0Q2yWR8d++fWuOj4//cJkWe9kMcl9TBBD0pkgzjjoBK4B1l2LZqk0crGurTpB2opWsQd/d3f21Dl0ERdamS9Vb5ZJ2cJmd9arY7Odeu7HP+/fvi73w5aq6LE3z9Yhry8bT2QFxuxtyj7AUn63IN93x6Ic7n8mPAIKeX86TibhO+7QTQhOTxnqB19wlrtdYbf1cBPH169d9LXPTfD3ifknq1gGxXRIRc5n8trCwUGBDzNt6ehi3FwEEvRchfh40Ac3lSJo7tPULLQQf+vW9yuf6bcX7WJlwWwdERFxeC8jP5LWOFfPO7kmVmLkXAr4JIOi+CWPfKwHt96tencX4HwRua8X3mvjWVL7dE/6s04g5D3DoBBD00DOEfz0JNPVHvqcj3NA3AWnFy45zUrlXEXWfcx6kG2DnLYQyH6FvwHwwCwIIehZpTj9IRD3+HLs5tDPiZ2dniwlpnTPRNedPxE+OCCDwHwEEnSchGQKuIMgscZnhzRUXAcmhVMaHh4e/ZsTbCDrPs7fzJ3xW6XHRw9vcCSDouT8BicUvgjA0NFQsMZJ9tbniJSBteFn7LTm9vr4uZsZLC9wuMZPDUKRNz3rweHOM57oEEHRdnlgLgIDMSD46OjIXFxdGWrdcaRBwz7N3I0LQ08gvUdQngKDXZ4iFwAiImIuo95pcFZjbuFOCQOcSMyarlYDGLdkQQNCzSXU+gTJBLp9cEykEIPCbAILO05AkAVfUNc7TThISQUEAAkkRQNCTSifBuARE1MfHx1XO04YsBCAAgdAJIOihZwj/ahHQPE+7liN8GAIQgIBnAgi6Z8CYb5+APXhle3vbLC8vt+8QHkAAAhDwQABB9wAVk2ER0DylK6zI8AYCEIDAbwIIOk9DFgTsKV1U6VmkmyAhkCUBBD3LtOcXNFV6fjknYgjkRgBBzy3jGcdrq/TNzU2zurqaMQlChwAEUiSAoKeYVWLqSkCWscke4V+/fjWjo6OQggAEIJAMAQQ9mVQSSBkCcqDH5ORkIeZnZ2fF0ZxcEIAABFIggKCnkEViqESAZWyVcHEzBCAQCQEEPZJE4aYeASbI6bHEEgQgEA4BBD2cXOBJgwRYxtYgbIaCAAQaIYCgN4KZQUIjQJUeWkbwBwIQqEsAQa9LkM9HS4AqPdrU4TgEIHALAQSdxyJbAlTp2aaewCGQJAEEPcm0ElRZAlTpZUlxHwQgEDoBBD30DOGfVwJU6V7xYhwCEGiQAILeIGyGCpMAVXqYecErCECgGgEEvRov7k6QAFV6gkklJAhkSABBzzDphPw3Aap0ngoIQCB2Agh67BnEfxUCVOkqGDECAQi0SABBbxE+Q4dFwFbpu7u7ZmlpKSzn8AYCEIBADwIIOo8IBP4nIFX60NCQmZiYMKenp3CBAAQgEBUBBD2qdOGsbwILCwvm6OjIXFxcmOHhYd/DYR8CEICAGgEEXQ0lhlIgIGIuor69vW2Wl5dTCIkYIACBTAgg6JkkmjDLEWByXDlO3AUBCIRHAEEPLyd41DKBtbU1s7W1ZRYXF83+/n7L3jA8BCAAgXIEEPRynLgrEwLn5+dmZGTkV7QnJydmZmYmk+gJEwIQiJkAgh5z9vDdCwFZvnZ1dWUGBweL5WsDAwNexsEoBCAAAU0CCLomTWxBAAIQgAAEWiKAoLcEnmEhAAEIQAACmgQQdE2a2IIABCAAAQi0RABBbwk8w0IAAhCAAAQ0CSDomjSxBQEIQAACEGiJAILeEniGhQAEIAABCGgSQNA1aWILAhCAAAQg0BIBBL0l8AwLAQhAAAIQ0CSAoGvSxBYEIAABCECgJQIIekvgGRYCEIAABCCgSQBB16SJLQhAAAIQgEBLBBD0lsAzLAQgAAEIQECTAIKuSRNbEIAABCAAgZYI/AuhCnjOWU+LvAAAAABJRU5ErkJggg==");
     const MyCheckBoxCell = (props) => (
         <CheckboxCell {...props}/>
       );
@@ -171,7 +173,18 @@ function OvenTest(props) {
       const handleIndexPage = (e) => {
         SetFirstPage(current => !current);
       };
-
+      const [openSignForm, setOpenSignForm] = useState(false);
+      const handleCancelSignForm = () => {
+        setOpenSignForm(false);
+      };
+  
+      const OpenSignForm = () =>{
+        setOpenSignForm(true);
+      }
+  
+      function SigImageCallback1(str) {
+        SetCalibratorSignature(str);
+      }
  
     return (
         <>
@@ -188,16 +201,17 @@ function OvenTest(props) {
                         <Label>הידרה:</Label>    
                         <Input></Input>
                     </div>
-                <Button className='nextBtn' onClick={handleIndexPage}>{firstPage ? "הבא": "הקודם"}
+                <Button className='nextOrPrevBtn' onClick={handleIndexPage}>{firstPage ? "הבא": "הקודם"}
                         <span className={firstPage ? "k-icon k-i-arrow-chevron-left" : "k-icon k-i-arrow-chevron-right"}></span>
                 </Button> 
-                 <div className='viewReport2'>
-            <Button className='createReportBtn' themeColor={"success"}>הנפק דוח</Button>
-            </div>
+                <div className='viewCreateReport'>
+                       <Button themeColor={"success"}>הנפק דוח</Button>
+                </div>
             </div>
           </div>
-         
+             {/* Display Report button*/}
            <DispalyReportContainer></DispalyReportContainer>   
+
            {
              firstPage ?
                 <div className='divPanelBarItem'>
@@ -214,8 +228,9 @@ function OvenTest(props) {
      
             <div className='tabStripDiv'>
             <div className='flexrow'>
-                 <div className='flexChooseChannelsCol'>
-         
+           
+                 {/* channel selection div */}
+                <div className='divChooseChannelsCol'>       
                     <PanelBar>
                         <PanelBarItem expanded={true} title="בחירת ערוצים">
                              <div className='divPanelBarItem'>
@@ -229,7 +244,7 @@ function OvenTest(props) {
                                             <GridColumn field="Active" title="ערוץ פעיל" cell={MyCheckBoxCell}/>  
 
                                             </Grid>
-                                            <div className='arrowsChannels'>
+                                            <div>
                                                     <Button icon='arrow-60-up'></Button> 
                                                     <Button icon='arrow-60-down'></Button>
                                             </div>
@@ -238,14 +253,15 @@ function OvenTest(props) {
                                 </div>
                         </PanelBarItem>
                     </PanelBar>
-
                 </div>
-                <div className='flexDeviceDetailsCol'>
-                <PanelBar>
+                
+                {/* Device details div */}
+                <div className='divDeviceDetails'>
+                    <PanelBar>
                         <PanelBarItem expanded={true} title="פרטי המכשיר המכוייל">
                          <div className='divPanelBarItem'>
                             <div className='flexrow'>
-                                    <div className='flexcolumnLarge'>
+                                    <div>
                                         <div className='flexControl'>
                                             <div>
                                                 <Label>זיהוי הפריט המכוייל:</Label>    
@@ -287,7 +303,7 @@ function OvenTest(props) {
                                             </div>                                  
                                         </div>
                                     </div>
-                                    <div className='flexcolumnLarge'>
+                                    <div>
                                         <div>
                                             <Label>תיאור הפריט ומצבו:</Label> 
                                             <div className='flexControl'>    
@@ -323,6 +339,8 @@ function OvenTest(props) {
             </div><br/>
                 <div className='flexrow'>
                     <div className='flexcolumn'>
+
+                        {/*costumer details div */}
                         <PanelBar>
                           <PanelBarItem expanded={true} title="פרטי הלקוח">
                             <div className='divPanelBarItem'>
@@ -341,27 +359,33 @@ function OvenTest(props) {
                                     <Input></Input>
                                 </div><br/>
                             </div>
-                        </PanelBarItem>
-                    </PanelBar>
+                          </PanelBarItem>
+                       </PanelBar>
                   </div>
-                        <div className='flexcolumn'>
-                        <PanelBar>
-                          <PanelBarItem expanded={true} title="פרטי הכייל/מאשר">
+                
+                  <div className='flexcolumn'>  
+                  {/* Calibration details Div */}
+                    <PanelBar>
+                        <PanelBarItem expanded={true} title="פרטי הכייל/מאשר">
                             <div className='divPanelBarItem'>
                                 <div className='flexControl'>
                                         <div>
                                             <Label>שם העובד המכייל:</Label>    
                                             <ComboBox></ComboBox>
                                         </div>
-                                        <div >
-                                            <Label>חתימה:</Label>    
-                                            <div className='flexControl'>
-                                                <Input></Input>&nbsp;
-                                                <Button themeColor={"primary"}>חתימה</Button>
-                                            </div>
+                                        <div>
+                                            <Label>חתימה:</Label>
+                                             <div className='flexrow'>
+                                                    <div className='flexcolumn'>
+                                                       <Button themeColor={"primary"} onClick={OpenSignForm}>חתימה</Button>
+                                                    </div>
+                                                    <div className='flexcolumn'>
+                                                      <img src={calibratorSignature ? "data:image/png;base64," + calibratorSignature : ''} className='img-sign' alt='' /> &nbsp;
+                                                   </div>   
+                                            </div>   
                                         </div>
-                                    </div>
-                                    <div className='flexControl'>
+                                </div>
+                                <div className='flexControl'>
                                         <div>
                                             <Label>שם מאשר התעודה:</Label>    
                                             <ComboBox></ComboBox>
@@ -373,24 +397,22 @@ function OvenTest(props) {
                                                 <Button themeColor={"primary"}>חתימה</Button>
                                             </div>
                                         </div>
-                                    </div>
+                                </div>
                             </div>   
                         </PanelBarItem>
                     </PanelBar>
                             
 
-                        </div>
-            </div><br/>
-           
-
-
-
+                  </div>
+                </div><br/>
+                
                 <div className='flexrow'>
-                        <div className='flexRightTemperturacolumn'>
+                    {/* Calibration areas */}
+                        <div className='divCalibrationAreas'>
                             <PanelBar>
                                 <PanelBarItem expanded={true} title="תחומי כיול">
                                     <div className='divPanelBarItem'>
-                                        <div className='divTemperura'>
+                                        <div>
                                             <Grid
                                                 data={TemperuraData}>
                                                     <GridColumn field="Type" title="סוג" width={"100px"}/>
@@ -404,7 +426,9 @@ function OvenTest(props) {
                             </PanelBar>
                           
                         </div>
-                        <div className='flexLeftTemperturacolumn'>
+
+                        {/* Environmental conditions during calibration */}
+                        <div className='divEnvironmentalconditions'>
                             <PanelBar>
                                 <PanelBarItem expanded={true} title="תנאי סביבה בעת הכיול" selected={false}>
                                     <div className='divPanelBarItem'>
@@ -465,10 +489,9 @@ function OvenTest(props) {
                         <PanelBarItem expanded={true} title="תחומים">
                           <br/>
                             <div className ='flexrow'>
-                                <div className='flexRightcolumn'>
-                                    <div>
-                                        
-                                        <Button themeColor={"primary"}>תחום חדש</Button>
+                                <div className='flexRightcolumnRanges'>
+
+                                     <Button themeColor={"primary"}>תחום חדש</Button>
                                                     <div className='divGridRange'>
                                                             <Grid
                                                                 data={OvenRangeData}>
@@ -478,9 +501,8 @@ function OvenTest(props) {
                                                                 <GridColumn field="Range" title="מחיקה" cell={MyDeleteRange} />
                                                             </Grid>
                                                     </div>
-                                    </div> 
                                 </div>
-                                <div className='flexMiddlecolumn'>
+                                <div className='flexMiddlecolumnRanges'>
                                 <PanelBar>
                                     <PanelBarItem expanded={true} title="נתונים כללים בתחום">
                                         <div className='divPanelBarItem'>
@@ -693,8 +715,11 @@ function OvenTest(props) {
                  </div>
                 </div>
               }  
-           
-       
+           {/* Opening a digital signature dialog */}
+           {openSignForm && (
+                 <SignWeb signData={calibratorSignature} parentCallback={ SigImageCallback1 } closeSignWeb={handleCancelSignForm}  ></SignWeb>
+         
+          )}
         
         
        </>
